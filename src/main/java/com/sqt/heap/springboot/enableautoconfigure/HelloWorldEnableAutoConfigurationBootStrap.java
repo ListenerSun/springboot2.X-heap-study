332a406b3b077e368e3d6bf3404e32bb08b80822
sunqingtian <sunqingtian@heplat.com>
2020-04-29 15:27:34 +0800

    update .gitgnore

---
+++ src/main/java/com/sqt/heap/springboot/enableautoconfigure/HelloWorldEnableAutoConfigurationBootStrap.java
@@ -0,0 +1,9 @@
+package com.sqt.heap.springboot.enableautoconfigure;
+
+/**
+ * @Description:
+ * @author: ListenerSun(男, 未婚) 微信:810548252
+ * @Date: Created in 2020-04-29 14:23
+ */
+public class HelloWorldEnableAutoConfigutationBootStarp {
+}
